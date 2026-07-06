1393831c23abe57cb0cc3bcf1e76c9021adf98ec
Lightshadow <parthagashe27@gmail.com>
2026-07-06 14:53:26 +0530

    Add Day 23 - Turtle Crossing Game (main loop, collision, level system)
    Implement turtle crossing game logic with player movement, car creation, and collision detection.

---
+++ day-23/turtle_crossing.py
@@ -0,0 +1,44 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.move, "w")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #detecting collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            
+            scoreboard.game_over()
+
+
+
+    #detecting end of level.
+    if player.ycor() > 280:
+        
+        player.next_level()
+        scoreboard.level()
+        car_manager.level_up()
+
+screen.exitonclick()
+    
